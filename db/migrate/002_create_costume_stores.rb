d82deb3d14680d78f8f4f0e6c2acd8765d44dca1
Benjamin Jones <bljones36@gmail.com>
2018-11-08 03:19:09 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.intenger :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.integer :opening_time
-      t.integer :closing_time
+      t.datetime :opening_time
+      t.datetime :closing_time
       t.timestamps
     end
   end
