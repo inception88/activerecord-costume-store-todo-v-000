4930626d47ba397ea4a27e0987973a3bb34a83fb
Benjamin Jones <bljones36@gmail.com>
2018-11-08 03:12:30 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :price
       t.string :size
       t.string :image_url
-      t.string :timestamps
+      t.timestamps
     end
   end
 end
